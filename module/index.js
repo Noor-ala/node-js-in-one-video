c6b3ca7e5fcf0224df5458402d4ee45d4f3f6273
noor <noor36331@gmail.com>
2023-03-30 02:18:03 +0530

    first commit

---
+++ module/index.js
@@ -0,0 +1,6 @@
+let a = "noor";
+console.log(a);
+const add=require("./sub")
+// require("./add")
+const sum = add(2,3)
+console.log(sum);
---
+++ module/sub.js
@@ -0,0 +1,6 @@
+function add(a,b){
+    return a - b; 
+}
+// const sum =add(8,1);
+// console.log(sum); 
+module.exports =add
