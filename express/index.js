bf400ada4263002d0302b2a28e0f089897983735
noor <noor36331@gmail.com>
2023-04-06 02:01:08 +0530

    change

--- express/index.js
+++ express/index.js
@@ -2,16 +2,26 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const pathdir=path.join(__dirname,'public')
-app.use(express.static(pathdir))
+//const pathdir=path.join(__dirname,'public')
+//app.use(express.static(pathdir))
+
+app.set("view engine","ejs");
+console.log(app.get("view engine"));
+console.log("views");
+
 app.get("/",(req,res)=>{
-    res.sendFile(`${pathdir}/inde.html`);
+    //res.sendFile(`${pathdir}/inde.html`);
+    res.render("inde",{
+        title:"index"
+    });
 });
 app.get("/about",(req,res)=>{
-    res.sendFile(`${pathdir}/home.html`);
+    //res.sendFile(`${pathdir}/home.html`);
+    res.render("home");
+    title:"home"
 });
 app.get("/download",(req,res)=>{
-    res.download(`${pathdir}/inde.html`)
+   // res.download(`${pathdir}/inde.html`)
 })
 
 app.listen(3000,()=>{
